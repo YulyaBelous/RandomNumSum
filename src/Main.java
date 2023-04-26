88b0edee3f1520958c171153f8da505d3e74c2b6
Yuliya <yulya.al.belous@gmail.com>
2023-04-26 22:11:57 +0300

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class Main {
-    static final int sum = 100;
-    static final int count = 10;
+    static final long sum = 100;
+    static final long count = 10;
     public static void main(String[] args) {
-        ArrayList<Integer> numbers;
+        ArrayList<Long> numbers;
         RandomNumber randomNum = new RandomNumber();
         for (int i = 0; i< 100; i++) {
-            numbers = randomNum.randomInt(sum, count);
+            numbers = randomNum.randomUniform(sum, count);
             System.out.print("Generate: ");
-            for(Integer key: numbers) {
-                System.out.print(key + " ");
+            for(Long key: numbers) {
+                System.out.print(key + " " );
             }
             System.out.println();
         }
--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,8 +1,24 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.LongStream;
 
 public class RandomNumber {
-    public ArrayList<Integer> randomInt(int sum, int count) {
+    public ArrayList<Long> randomUniform(long sum, long count) {
+        Random random = new Random();
+        long [] bufNum = LongStream.generate(() -> (long) (random.nextDouble() * sum)).limit(count).toArray();
+        long sumBuf = Arrays.stream(bufNum).sum();
+        ArrayList<Long> numbers = new ArrayList<>();
+        int sumNumbers = 0;
+        for(int i = 0; i < count-1; i++) {
+            numbers.add(bufNum[i] * sum / sumBuf);
+            sumNumbers += numbers.get(i);
+        }
+        numbers.add(sum - sumNumbers);
+        return numbers;
+    }
+
+    public ArrayList<Integer> randomGaussian(int sum, int count) {
         ArrayList<Integer> numbers = new ArrayList<>();
         Random r = new Random();
         int bufNum, bufSum = 0;
